a445eb20c60b35426eb3f27949a0c08d00c55bb8
carter-et <acrealabamawoo@gmail.com>
2020-04-21 17:34:26 -0400

    everything needed for deployment has been added

---
+++ floorman/src/components/timer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="minutes">
+      {{minutes}}
+    </div>
+    <div class="seconds">
+      {{seconds}}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component';
+@Component
+export default class timer extends Vue{
+  private minutes: number = 15;
+  private seconds: number = 0;
+}
+</script>
+
+<style lang="scss">
+
+</style>
